dc3a74a5ccc0a08a9aaba8f99d67d127bf47c9b7
Shivraj Roy <shivrajroy78@gmail.com>
2025-05-02 23:42:38 +0530

    feat: add user input handling and focus management...

--- src/components/TypeZone.tsx
+++ src/components/TypeZone.tsx
@@ -1,5 +1,5 @@
 import { generate } from "random-words";
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useTestMode } from "../context/TestMode";
 import TimeCounter from "./TimeCounter";
 
@@ -8,13 +8,30 @@ const TypeZone = () => {
    const { testTime } = useTestMode() as { testTime: number }; // Ensure the type matches your context
    const [counter, setCounter] = useState<number>(testTime);
 
+   const inputRef = useRef<HTMLInputElement>(null);
+
+   const handleUserInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      console.log(e.key);
+   };
+
+   const focusInput = () => {
+      inputRef.current?.focus();
+   };
+
+   useEffect(() => {
+      focusInput();
+   }, []);
+
    useEffect(() => {
       setCounter(testTime);
    }, [testTime]);
 
    return (
       <>
-         <div className="max-w-full mx-auto overflow-hidden self-start  mb-16">
+         <div
+            className="max-w-full mx-auto overflow-hidden self-start  mb-16"
+            onClick={focusInput}
+         >
             <TimeCounter countDown={counter} />
             <div className="text-3xl flex flex-wrap leading-12 tracking-wide">
                {words.map((word, wordIndex) => (
@@ -30,6 +47,12 @@ const TypeZone = () => {
                   </span>
                ))}
             </div>
+            <input
+               type="text"
+               className="hidden"
+               onKeyDown={handleUserInput}
+               ref={inputRef}
+            />
          </div>
       </>
    );
